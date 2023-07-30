1c23cb72eec1e38b116f305a14404807f57795c5
Dennis Cheung <dennis@Dennis-MacBook-Air.local>
2023-07-30 00:32:36 -0700

    updated projects section to include selected projects

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,7 +1,319 @@
 import React from 'react';
+import Image from 'next/image';
 
 const ProjectsSection = () => {
-  return <div>ProjectsSection</div>;
+  return (
+    <section
+      id='projects'
+      className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'
+      aria-label='Selected projects'
+    >
+      <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+        <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>
+          Projects
+        </h2>
+      </div>
+      <div>
+        <ul className='group/list'>
+          <li className='mb-12'>
+            <div className='group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
+              <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></div>
+              <div className='z-10 sm:order-2 sm:col-span-6'>
+                <h3>
+                  <a
+                    className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
+                    href='https://github.com/Dennis-JS/R2D2-Lite'
+                    target='_blank'
+                    rel='noreferrer'
+                    aria-label='R2D2 Lite'
+                  >
+                    <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
+                    <span>
+                      R2-D2 Lite
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        className='inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px'
+                        aria-hidden='true'
+                      >
+                        <path
+                          fill-rule='evenodd'
+                          d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z'
+                          clip-rule='evenodd'
+                        ></path>
+                      </svg>
+                    </span>
+                  </a>
+                </h3>
+                <p className='mt-2 text-sm leading-normal'>
+                  An chatbot powered by OpenAI API built with React, FastAPI,
+                  LangChain to auto-generate personalized email sequences.
+                </p>
+                <ul
+                  className='mt-2 flex flex-wrap'
+                  aria-label='Technologies used'
+                >
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      OpenAI API
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      React
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      FastAPI
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      LangChain
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      JavaScript
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      Python
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <Image
+                className='rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1'
+                src='/website.png'
+                width={200}
+                height={48}
+                alt='picture of portfolio'
+              />
+            </div>
+          </li>
+          <li className='mb-12'>
+            <div className='group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
+              <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></div>
+              <div className='z-10 sm:order-2 sm:col-span-6'>
+                <h3>
+                  <a
+                    className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
+                    href='https://github.com/MyBookcase/Scratch_Project'
+                    target='_blank'
+                    rel='noreferrer'
+                    aria-label='MyBookcase'
+                  >
+                    <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
+                    <span>
+                      MyBookcase
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        className='inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px'
+                        aria-hidden='true'
+                      >
+                        <path
+                          fill-rule='evenodd'
+                          d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z'
+                          clip-rule='evenodd'
+                        ></path>
+                      </svg>
+                    </span>
+                  </a>
+                </h3>
+                <p className='mt-2 text-sm leading-normal'>
+                  A visualizer to store collection of books owned and read by
+                  individual users.
+                </p>
+                <ul
+                  className='mt-2 flex flex-wrap'
+                  aria-label='Technologies used'
+                >
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      React
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      Node
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      MongoDB
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      JavsScript
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      TypeScript
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <Image
+                className='rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1'
+                src='/website.png'
+                width={200}
+                height={48}
+                alt='picture of portfolio'
+              />
+            </div>
+          </li>
+          <li className='mb-12'>
+            <div className='group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
+              <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></div>
+              <div className='z-10 sm:order-2 sm:col-span-6'>
+                <h3>
+                  <a
+                    className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
+                    href='https://github.com/Dennis-JS/portfolio-website'
+                    target='_blank'
+                    rel='noreferrer'
+                    aria-label='Jobbr'
+                  >
+                    <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
+                    <span>
+                      Jobbr
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        className='inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px'
+                        aria-hidden='true'
+                      >
+                        <path
+                          fill-rule='evenodd'
+                          d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z'
+                          clip-rule='evenodd'
+                        ></path>
+                      </svg>
+                    </span>
+                  </a>
+                </h3>
+                <p className='mt-2 text-sm leading-normal'>
+                  An app to keep track of jobs that users have applied to as
+                  well as their progression for each application.
+                </p>
+                <ul
+                  className='mt-2 flex flex-wrap'
+                  aria-label='Technologies used'
+                >
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      Next.js
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      JavaScript
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      TypeScript
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      Tailwind CSS
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <Image
+                className='rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1'
+                src='/website.png'
+                width={200}
+                height={48}
+                alt='picture of portfolio'
+              />
+            </div>
+          </li>
+          <li className='mb-12'>
+            <div className='group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
+              <div className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></div>
+              <div className='z-10 sm:order-2 sm:col-span-6'>
+                <h3>
+                  <a
+                    className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
+                    href='https://github.com/Dennis-JS/portfolio-website'
+                    target='_blank'
+                    rel='noreferrer'
+                    aria-label='Portfolio Website'
+                  >
+                    <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
+                    <span>
+                      Portfolio Website
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        className='inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px'
+                        aria-hidden='true'
+                      >
+                        <path
+                          fill-rule='evenodd'
+                          d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z'
+                          clip-rule='evenodd'
+                        ></path>
+                      </svg>
+                    </span>
+                  </a>
+                </h3>
+                <p className='mt-2 text-sm leading-normal'>
+                  My personal portfolio website build with Next.JS and Tailwind
+                  CSS
+                </p>
+                <ul
+                  className='mt-2 flex flex-wrap'
+                  aria-label='Technologies used'
+                >
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      Next.js
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      JavaScript
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      TypeScript
+                    </div>
+                  </li>
+                  <li className='mr-1.5 mt-2'>
+                    <div className='flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 '>
+                      Tailwind CSS
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <Image
+                className='rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1'
+                src='/website.png'
+                width={200}
+                height={48}
+                alt='picture of portfolio'
+              />
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default ProjectsSection;
